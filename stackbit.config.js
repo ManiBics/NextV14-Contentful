abb87c705d0e27b3e6989215fd15536838f4b1d3
ManiBics <manibicsglobal@gmail.com>
2024-01-10 22:04:38 +0530

    Modified stackbit config

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,17 +1,18 @@
 import { ContentfulContentSource } from "@stackbit/cms-contentful";
-import { defineStackbitConfig } from "@stackbit/types";
 
-export default defineStackbitConfig({
+const config = {
   stackbitVersion: "~0.6.0",
   ssgName: "nextjs",
   nodeVersion: "18",
   contentSources: [
     new ContentfulContentSource({
-      spaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-      environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || "master",
-      previewToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_TOKEN,
-      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_TOKEN,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     }),
   ],
   modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }],
-});
+};
+
+export default config;
