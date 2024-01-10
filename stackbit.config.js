3e8bf9b880ea9ab8658302258dacfbbcd6effd90
ManiBics <manibicsglobal@gmail.com>
2024-01-10 21:52:39 +0530

    updated stackbit config

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,15 +1,17 @@
-import { ContentfulContentSource } from '@stackbit/cms-contentful';
-import { defineStackbitConfig } from '@stackbit/types';
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
+import { defineStackbitConfig } from "@stackbit/types";
 
 export default defineStackbitConfig({
-    stackbitVersion: '~0.6.0',
-    contentSources: [
-        new ContentfulContentSource({
-            spaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-            environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || 'master',
-            previewToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_TOKEN,
-            accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_TOKEN
-        })
-    ],
-    modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }]
+  stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
+  nodeVersion: "18",
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+      environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || "master",
+      previewToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ],
+  modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }],
 });
