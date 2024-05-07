6de2124d3234905fd69547ed954059419c0006f0
ManiBics <manibicsglobal@gmail.com>
2024-05-07 20:07:39 +0530

    Show active menu in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,11 +7,12 @@ import Slide from "@mui/material/Slide";
 import { Stack } from "@mui/material";
 import Image from "next/image";
 import SearchIcon from "@mui/icons-material/Search";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Header(props) {
   const { window } = props;
   const router = useRouter();
+  const pathname = usePathname();
 
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
@@ -76,13 +77,17 @@ export default function Header(props) {
               className="h-28 px-28 w-full"
               justifyContent="space-between"
             >
-              <div className="flex h-full items-center">
+              <div
+                className="flex h-full items-center "
+                onClick={() => handleNavigate("/")}
+              >
                 <Image
                   data-sb-object-id={props.logo.id}
                   src={props.logo.src}
                   alt={props.logo.alt}
                   height={38}
                   width={162}
+                  className="cursor-pointer"
                 />
               </div>
 
@@ -102,7 +107,11 @@ export default function Header(props) {
                     <div
                       key={menu.id}
                       data-sb-field-path={`${menu.id}:title`}
-                      className="font-medium text-lg hover:text-[#019881] cursor-pointer"
+                      className={`font-medium text-lg hover:text-[#019881] cursor-pointer ${
+                        pathname === menu.url
+                          ? "text-[#019881]"
+                          : "text-inherit"
+                      }`}
                       onClick={() => handleNavigate(menu.url)}
                     >
                       {menu.title}
